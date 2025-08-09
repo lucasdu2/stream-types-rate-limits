8fbe170ed7054a00d06b69b418ff08035f6610fa
Lucas Du <lucaszdu@gmail.com>
2025-08-09 12:40:46 -0700

    Save some quick notes in paper draft
    I should probably put them in my real notebook later...

--- ratelimitsub-proto1/rlsub-proto1/lib/rlsub.ml
+++ ratelimitsub-proto1/rlsub-proto1/lib/rlsub.ml
@@ -11,31 +11,24 @@ type typ =
   | TypSum of typ * typ * rl
   | TypPar of typ * typ * rl
   | TypStar of typ * rl
-let typ_rl (t: typ) =
-  match t with
-  | TypInt(r) -> r
-  | TypConcat(_, _, r) -> r
-  | TypSum(_, _, r) -> r
-  | TypPar(_, _, r) -> r
-  | TypStar(_, r) -> r
 
 (* =========================   SUBTYPING   ================================== *)
 (* Two pass approach: push outer refinements inwards, then inner refinements
    outwards when you come back up from the recursive calls. *)
-let rec subtype typS typT =
-  match (typS, typT) with
-  | (TypInt(rl1), TypInt(rl2)) ->
-     (match (rl1, rl2) with
-      | (Uniform(r1), Uniform(r2)) -> uniform_rate_sub r1 r2
-      | (_, _) -> false)
-  | (TypConcat(s1, t1, rl1), TypConcat(s2, t2, rl2)) ->
-     (match (rl1, rl2) with
-      |
-     )
-  | (_, _) -> false
-and normalize typS typT =
-  |
-and uniform_rate_sub (r1: rate) (r2: rate) =
+
+(* TODO: How do we define a normalized type? *)
+let rec normalize_super (s: typ) =
+  match s with
+  | TypInt(r) -> TypInt(r)
+  | TypConcat(s1, s2, r) ->
+     (if r = Uniform(NoLimit) then
+        TypConcat(normalize_super(s1), normalize_super(s2), r)
+      else
+        )
+
+
+
+let uniform_rate_sub (r1: rate) (r2: rate) =
   match (r1, r2) with
   | (_, NoLimit) -> true
   | (NoLimit, _) -> false
@@ -47,53 +40,3 @@ and uniform_rate_sub (r1: rate) (r2: rate) =
       else
         (let bound = (n2 / ((t2 / t1) + 1)) in
          if n1 <= bound then true else false))
-and uniform_concat_expand s1 t1 rl1 s2 t2 =
-
-
-(*   (\* TODO: Add proper bodies to each case statement here. *\) *)
-(*   | (Uniform(_t1), Uniform(_t2)) -> false *)
-(*   | (Segment(_t1), Segment(_t2)) -> false *)
-(*   | (Uniform(_t1), Segment(_t2)) -> false *)
-(*   | (Segment(_t1), Uniform(_t2)) -> false *)
-(* and uniform_rr_sub rr1 rr2 = *)
-(*   match (rr1, rr2) with *)
-(*   | (_, Top) -> true *)
-(*   | (Rate(rate1), Rate(rate2)) -> *)
-(*      if rate2.window <= rate1.window then *)
-(*        if rate1.events <= rate2.events then true else false *)
-(*      else (\* rate2.window > rate1.window *\) *)
-(*        (\* NOTE: Model everything with integers --- indeed, natural numbers --- *)
-(*           here. This is expressive enough, since each increment can correspond to *)
-(*           a tick in whatever discrete time-keeping system we are talking about. *)
-(*           This is probably also how we'll formalize the semantics of our *)
-(*           subtyping rules. *\) *)
-(*        (let bound = (rate2.events / ((rate2.window / rate1.window) + 1)) in *)
-(*         if (rate1.events <= bound) then true else false) *)
-(*   | (Top, _) -> false *)
-(* and uniform_sub typS typT = *)
-(*   (=) typS typT || *)
-(*     match (typS, typT) with *)
-(*     (\* Basic subtyping rules on rate refinements only *\) *)
-(*     | ({base = b1; refine = rr1}, {base = b2; refine = rr2}) -> *)
-(*        (uniform_rr_sub rr1 rr2) && (subtype b1 b2) *)
-(*     | (TypConcat(rr1, typS1, typT1), TypConcat(rr2, typS2, typT2)) -> *)
-(*        (uniform_rr_sub rr1 rr2) && (subtype typS1 typS2) && (subtype typT1 typT2) *)
-(*     | (TypSum(rr1, typS1, typT1), TypSum(rr2, typS2, typT2)) -> *)
-(*        (uniform_rr_sub rr1 rr2) && (subtype typS1 typS2) && (subtype typT1 typT2) *)
-(*     | (TypPar(rr1, typS1, typT1), TypPar(rr2, typS2, typT2)) -> *)
-(*        (uniform_rr_sub rr1 rr2) && (subtype typS1 typS2) && (subtype typT1 typT2) *)
-(*     | (TypStar(rr1, typS1), TypStar(rr2, typS2)) -> *)
-(*        (uniform_rr_sub rr1 rr2) && (subtype typS1 typS2) *)
-(*     (\* Subtyping rules for "expand" and "factor" *\) *)
-(*     (\* NOTE/TODO: Seems like we need to figure out how to handle nested rate *)
-(*        refinements fairly urgently. *\) *)
-(*     | (TypConcat(l_rr, typS1, typT1), TypConcat(Top, typS2rr, typT2rr)) -> *)
-(*        let r_rr1 = rltyp_rr typS2rr in *)
-(*        let r_rr2 = rltyp_rr typT2rr in *)
-(*        (uniform_rr_sub l_rr r_rr1) && (uniform_rr_sub l_rr r_rr2) && *)
-(*          (subtype typS1 ) *)
-(*     | (_, _) -> false *)
-
-(* and segment_sub typS typT *)
-
-(* and convert_sub *)
