79c627e4fa40319fdc38391a1c6e6e4f577d6248
Lucas Du <lucaszdu@gmail.com>
2025-08-25 20:18:51 -0700

    Continue with port; some problems with borrows

--- ratelimitsub-proto2/src/main.rs
+++ ratelimitsub-proto2/src/main.rs
@@ -1,8 +1,12 @@
+#[derive(Copy, Clone, Debug)]
 struct Rate {
     events: usize,
     window: usize,
 }
+
 type RateRefine = Vec<Rate>;
+
+#[derive(Clone, Debug)]
 enum StreamTy {
     Int(RateRefine),
     Sum(Box<StreamTy>, Box<StreamTy>, RateRefine),
@@ -20,7 +24,7 @@ fn uniform_rate_sub(rate1: &Rate, rate2: &Rate) -> bool {
     }
 }
 
-fn uniform_rr_sub(refine1: &RateRefine, refine2: &RateRefine) -> bool {
+fn uniform_refine_sub(refine1: &RateRefine, refine2: &RateRefine) -> bool {
     let check_rate_sub = |rate1: &Rate| -> bool {
         for rate2 in refine2 {if uniform_rate_sub(rate1, rate2) {return true}};
         return false;
@@ -29,10 +33,29 @@ fn uniform_rr_sub(refine1: &RateRefine, refine2: &RateRefine) -> bool {
     return true;
 }
 
+fn merge_refine(mut inner_refine: &RateRefine, outer_refine: &RateRefine) -> RateRefine {
+    // TODO: Why do I need two borrows here?
+    let can_erase = |out_r: &&Rate| -> bool {
+        for in_r in inner_refine {if uniform_rate_sub(in_r, out_r) {return true}};
+        return false;
+    };
+    // TODO: Figure out functional weirdness here --- might just want to do
+    // this imperatively for now...
+    let mut to_add: RateRefine = outer_refine.into_iter().filter(can_erase).collect();
+    inner_refine.append(&to_add)
+}
+
 fn check_subtype (s1: &StreamTy, s2: &StreamTy) -> bool {
-    // TODO: Fix this borrow checker problem.
     match (s1, s2) {
-        (&StreamTy::Int(refine1), &StreamTy::Int(refine2)) => uniform_rr_sub(&refine1, &refine2),
+        (StreamTy::Int(refine1), StreamTy::Int(refine2)) => {
+            uniform_rr_sub(&refine1, &refine2)
+        },
+        (StreamTy::Sum(s1, s2, refine1), StreamTy::Sum(t1, t2, refine2)) => {
+            // TODO: Either need to figure out how to nicely pattern match on
+            // empty vectors or re-do the implementation a bit to run the push
+            // in (or "normalize") operation as part of check_subtype.
+            uniform_rr_sub(&refine1, &refine2) && check_subtype(s1, t1) && check_subtype(s2, t2)
+        },
         _ => false,
     };
    return false;
